de00bed23eae491d2b73e3740af5f927a603860f
mountiv <silviomavrel@gmail.com>
2023-02-24 14:09:15 +0100

    merge filtered human_img and new background_img

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import os
 import cv2
 import numpy as np
 import mediapipe as mp
+
 mp_selfie_segmentation = mp.solutions.selfie_segmentation
 selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation(model_selection=1)
 cap = cv2.VideoCapture(0)
+
+width = 900
+height = 600
+bg_img = cv2.imread(f'background_img/new/bg1.jpg')
+
 while True:
     frame = cv2.imread("./human_img/1.jpg")
     RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -12,8 +18,11 @@ while True:
     results = selfie_segmentation.process(RGB)
     # extract segmented mask
     mask = results.segmentation_mask
+    condition = np.stack((mask,) * 3, axis=-1) > 0.5
+    # bg_img = cv2.resize(bg_img, (width, height))
+    output_image = np.where(condition, frame, bg_img)
     # show outputs
-    cv2.imshow("mask", mask)
+    cv2.imshow("Output", output_image)
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1)
     if key == ord('q'):
--- resize.py
+++ resize.py
@@ -1,12 +1,14 @@
 import cv2
 import os
 
+width = 900
+height = 600
+
 listImg = os.listdir(f'background_img/old')
 # imgList = []
 for imgPath in listImg:
     img = cv2.imread(f'background_img/old/{imgPath}')
     # imgList.append(imgPath)
-    img = cv2.resize(img, (900, 500))
+    img = cv2.resize(img, (width, height))
     cv2.imwrite(f'background_img/new/'+imgPath, img)
-    print(*["Image", imgPath, "is resized to 900 X 500"])
-
+    print(*["Image", imgPath, f'is resized to {width} X {height}'])
