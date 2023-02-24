31752a4fb2c88f91004e80892b873515334ea688
mountiv <silviomavrel@gmail.com>
2023-02-24 12:59:58 +0100

    background image resizing implemented: resize.py

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-Dev OS: Windows 10
-Python version: 3.10.10     [download](https://www.python.org/downloads/release/python-31010/)
+- Dev OS: Windows 10
+- Python version: 3.10.10     [download](https://www.python.org/downloads/release/python-31010/)
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ mp_selfie_segmentation = mp.solutions.selfie_segmentation
 selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation(model_selection=1)
 cap = cv2.VideoCapture(0)
 while True:
-    frame = cv2.imread("./human_img/4.jpg")
+    frame = cv2.imread("./human_img/1.jpg")
     RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     # get the result
     results = selfie_segmentation.process(RGB)
