be402bc47912a374fc9b95add33bdc00646e2f8a
mountiv <silviomavrel@gmail.com>
2023-02-27 07:59:21 +0100

    updated UI styles

--- meetn_bonus_app/main.py
+++ meetn_bonus_app/main.py
@@ -1,4 +1,9 @@
+# 02-26 failed with class
 import sys
+import os
+import imghdr
+from pathlib import Path
+import random
 
 from PyQt6.QtGui import QPixmap, QIcon
 from PyQt6.QtWidgets import (
@@ -21,6 +26,7 @@ default_font = "calibri"
 class Window(QWidget):
     def __init__(self):
         super().__init__()
+        self.setFixedSize(1200, 700)
 
         self.setWindowIcon(QIcon('logo.png'))
         self.setWindowTitle("Meetn Bonus App")
@@ -43,19 +49,21 @@ class MainPane(QWidget):
 class NavigationPane(QWidget):
     def __init__(self, arg):
         super(NavigationPane, self).__init__()
-        layout = QVBoxLayout()
-        layout.addWidget(SubTitle("Select a Virtual Background:"))
+        self.background_image_path = ""
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(SubTitle("Select a Virtual Background:"))
         self.dir_name_edit = QLineEdit()
         self.dir_name_edit.setStyleSheet(
             "margin-top: 9px;"
             "background-color: #444444;"
         )
-        layout.addWidget(self.dir_name_edit, 2)
+        self.layout.addWidget(self.dir_name_edit, 2)
         self.openFolderBtn = QPushButton("Select Folder", self)
-        layout.addWidget(self.openFolderBtn, 2)
-        layout.addWidget(BackgroundImageGallery(), 2)
-        self.setLayout(layout)
-        self.setLayout(layout)
+        self.layout.addWidget(self.openFolderBtn, 2)
+        self.backgroundImageGallery = BackgroundImageGallery("", [])
+        self.layout.addWidget(self.backgroundImageGallery, 2)
+        self.setLayout(self.layout)
+        self.setLayout(self.layout)
         self.openFolderBtn.clicked.connect(self.openFolderButtonClicked)
 
     def openFolderButtonClicked(self):
@@ -63,9 +71,22 @@ class NavigationPane(QWidget):
 
     def openFileNamesDialog(self):
         dialog = QFileDialog(self)
-        bg_dir_path = str(dialog.getExistingDirectory(self, "Select Virtual Background Folder"))
-        if bg_dir_path:
-            self.dir_name_edit.setText(bg_dir_path)
+        background_image_directory_path = str(dialog.getExistingDirectory(self, "Select Virtual Background Folder"))
+        if background_image_directory_path:
+            self.dir_name_edit.setText(background_image_directory_path)
+
+            background_image_list = os.listdir(Path(background_image_directory_path))
+            background_image_list_new = []
+            for background_image_path in background_image_list:
+                my_path = f'{Path(background_image_directory_path)}\{background_image_path}'
+                isFile = os.path.isfile(my_path)
+                if (isFile):
+                    if (imghdr.what(my_path) != None):
+                        background_image_list_new.append(my_path)
+            self.backgroundImageGallery.close()
+
+            self.backgroundImageGallery = BackgroundImageGallery(background_image_directory_path, background_image_list_new)
+            self.layout.addWidget(self.backgroundImageGallery, 2)
 
 class SubTitle(QWidget):
     def __init__(self, title):
@@ -85,14 +106,20 @@ class SubTitle(QWidget):
 class CompositeScreen(QWidget):
     def __init__(self, path):
         super(CompositeScreen, self).__init__()
-        self.im = QPixmap("../basic_feature/human_img/1.jpg")
+        self.im = QPixmap(f"../basic_feature/human_img/2.jpg")
         self.im = self.im.scaled(720, 500)
         self.label = QLabel()
+        self.label.addActions
+        self.label.mousePressEvent = self.clickedMouse
         self.label.setPixmap(self.im)
 
         self.grid = QGridLayout()
         self.grid.addWidget(self.label, 1, 1)
         self.setLayout(self.grid)
+    def clickedMouse(self, event):
+        i = random.randint(1, 4)
+        self.im = QPixmap(f"../basic_feature/human_img/{i}.jpg")
+        self.label.setPixmap(self.im)
 
 class AvaliableCameras(QWidget):
     def __init__(self, wshat):
@@ -109,10 +136,18 @@ class AvaliableCameras(QWidget):
         self.setLayout(layout)
 
 class BackgroundImageGallery(QWidget):
-    def __init__(self):
+    def __init__(self, background_image_directory_path, background_image_list):
         super(BackgroundImageGallery, self).__init__()
         layout = QHBoxLayout()
         scrollArea = QScrollArea()
+        scrollArea.setStyleSheet(
+            "QScrollBar:vertical"
+            "{"
+                "border: none;"
+                "width: 7px;"
+                "margin: 0 0 0 0;"
+            "}"
+        )
         scrollArea.setWidgetResizable(True)
         scrollAreaWidgetContents = QWidget()
         gridLayout = QGridLayout(scrollAreaWidgetContents)
@@ -120,17 +155,14 @@ class BackgroundImageGallery(QWidget):
         scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
         scrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
 
-        gridLayout.addWidget(GalleryImage('../basic_feature/background_img/new/bg1.jpg'), 0, 0)
-        gridLayout.addWidget(GalleryImage('../basic_feature/background_img/new/bg2.jpg'), 0, 1)
-        gridLayout.addWidget(GalleryImage('../basic_feature/background_img/new/bg3.jpg'), 1, 0)
-        gridLayout.addWidget(GalleryImage('../basic_feature/background_img/new/bg1.jpg'), 1, 1)
-        gridLayout.addWidget(GalleryImage('../basic_feature/background_img/new/bg2.jpg'), 2, 0)
-        gridLayout.addWidget(GalleryImage('../basic_feature/background_img/new/bg3.jpg'), 2, 1)
-        gridLayout.addWidget(GalleryImage('../basic_feature/background_img/new/bg1.jpg'), 3, 0)
-        gridLayout.addWidget(GalleryImage('../basic_feature/background_img/new/bg2.jpg'), 3, 1)
-        gridLayout.addWidget(GalleryImage('../basic_feature/background_img/new/bg3.jpg'), 4, 0)
-        gridLayout.addWidget(GalleryImage('../basic_feature/background_img/new/bg1.jpg'), 4, 1)    
-        
+        i = 0
+        for background_image_path in background_image_list:
+            isFile = os.path.isfile(background_image_path)
+            if (isFile):
+                if (imghdr.what(background_image_path) != None):
+                    gridLayout.addWidget(GalleryImage(os.path.relpath(background_image_path)), int((i-i%2)/2), i%2)
+                    i += 1
+                    
         layout.addWidget(scrollArea)
         self.setLayout(layout)
 
@@ -139,8 +171,9 @@ class GalleryImage(QWidget):
         super(GalleryImage, self).__init__()
         layout = QHBoxLayout()
         label = QLabel()
+        print(background_image_path)
         pixmap = QPixmap(background_image_path)
-        pixmap = pixmap.scaled(140, 100)
+        pixmap = pixmap.scaled(160, 120)
         label.setPixmap(pixmap)
         layout.addWidget(label)
         self.setLayout(layout)
